2b1c5ad80055c47edc71d4b88995ca53ddbcffa5
Ahmed Rzeigui <ahmedrzeigui52@gmail.com>
2024-12-10 21:52:55 +0100

    Création classe Cercle

--- Cercle.hpp
+++ Cercle.hpp
@@ -1,1 +1,17 @@
 #include <iostream>
+
+class Cercle {
+
+private:
+  int x;
+  int y;
+  int w;
+  int h;
+
+public:
+  Cercle(int x1, int y1, int w1, int h1) : x(x1), y(y1), w(w1), h(h1) {}
+  Cercle(int cx, int cy, int rayon) {}
+  std::string toString() {
+    std::cout << x << " " << y << " " << w << " " << h << std::endl;
+  }
+};
