86f10ba6cf0d5e5b3554b624ce266decfbba6e3b
Ahmed Rzeigui <ahmedrzeigui52@gmail.com>
2024-12-11 11:16:50 +0100

    Ajout catch2.hpp et Update Point

--- Point.cpp
+++ Point.cpp
@@ -1,1 +1,7 @@
 #include "Point.hpp"
+
+void Point::setX(int a) { x = a; }
+void Point::setY(int b) { y = b; }
+
+int Point::getX() { return x; }
+int Point::getY() { return y; }
--- Point.hpp
+++ Point.hpp
@@ -4,4 +4,10 @@ class Point {
 private:
   int x;
   int y;
+
+public:
+  void setX(int a);
+  void setY(int b);
+  int getX();
+  int getY();
 };
